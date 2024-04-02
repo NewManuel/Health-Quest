b89de9cc3b07bef97bff359e88927c02c3507ed3
Nick Smith <nick.smith.asu@gmail.com>
2024-04-02 11:43:32 -0700

    Remove text from header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -10,8 +10,6 @@ const Header = () => {
   return (
     <header>
       <h1>HEALTH QUEST</h1>
-      <p>Hello {Auth.getProfile().data.username.charAt(0).toUpperCase() + Auth.getProfile().data.username.slice(1)}, we are so glad you're here. Tracking your habits is one of the first steps towards building a life full of health and vitality. </p>
-      <p>As you wind down for the day, take a moment to reflect on your experiences. How did today go? What were the highlights and challenges you encountered? Reflecting on your day can provide valuable insights into your habits and patterns.</p>
       <div className='header-buttons'>
         <Link to="/previous-entries" className="view-entries-button">
           View Previous Entries
