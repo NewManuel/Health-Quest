d766cf727ed8fee1771d57d2eb466b4a0ffd317f
Nick Smith <nick.smith.asu@gmail.com>
2024-04-01 17:38:09 -0700

    Add view previous entries button

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -12,9 +12,14 @@ const Header = () => {
       <h1>HEALTH QUEST</h1>
       <p>Hello {Auth.getProfile().data.username.charAt(0).toUpperCase() + Auth.getProfile().data.username.slice(1)}, we are so glad you're here. Tracking your habits is one of the first steps towards building a life full of health and vitality. </p>
       <p>As you wind down for the day, take a moment to reflect on your experiences. How did today go? What were the highlights and challenges you encountered? Reflecting on your day can provide valuable insights into your habits and patterns.</p>
-      <button className="logout-button" onClick={logout}>
-        Logout
-      </button>
+      <div className='header-buttons'>
+        <Link to="/previous-entries" className="view-entries-button">
+          View Previous Entries
+        </Link>
+        <button className="logout-button" onClick={logout}>
+          Logout
+        </button>
+      </div>
     </header>
   );
 };
