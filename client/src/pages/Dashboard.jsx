c75812239d8f66522d45a8dcaa0bd35f77b8b0c6
Nick Smith <nick.smith.asu@gmail.com>
2024-04-03 16:56:36 -0700

    Decouple the informational text out of the header

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import DashboardInformationalMessage from "../components/DashboardInfoMessage";
 import HealthQuestionnaire from "../components/HealthQuestionaire";
 
 const Dashboard = () => {
     return (
         <div className="main">
             <Header />
+            <DashboardInformationalMessage />
             <div className="dashboard-container">
                 <HealthQuestionnaire />
             </div>
