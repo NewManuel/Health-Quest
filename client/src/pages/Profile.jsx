41c0af21684a46cb588613d0e25bd529edbdb85f
Nick Smith <nick.smith.asu@gmail.com>
2024-04-04 20:23:16 -0700

    Add the Go Back Button

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,19 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ProfileComponent from "../components/Profile";
+import { useLocation } from 'react-router-dom';
+import GoBackButton from '../components/GoBackButton';
 
 
 const Profile = () => {
+  const location = useLocation();
   return (
     <div className="main">
       <Header />
       <ProfileComponent />
+      <div className="go-back-container">
+        {location.pathname !== '/dashboard' && <GoBackButton />}
+      </div>
       <Footer />
     </div>
   )
